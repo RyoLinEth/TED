ba34c4286f8f2570a6a96c88e41a3244007ae901
RyoLinEth <ryolindev@gmail.com>
2023-11-30 08:36:59 +0800

    Change CA

--- src/DataProvider.jsx
+++ src/DataProvider.jsx
@@ -7,7 +7,7 @@ export const DataProvider = ({ children }) => {
     //  BSC Contracts
     const USDTContractAddress = "0x55d398326f99059fF775485246999027B3197955"
     const USDContractAddress = "0x9aF7D79c9910A1Ea169deeDFfC757FAB4D5E7dfD"
-    const MinerContractAddress = "0x6b32341215dFEaa2CDAFa07806E558660fE5C326"
+    const MinerContractAddress = "0xC8EB91C20C835cefb9F3092C0B37642a9669f45a"
 
     const TEDAddress="0x7c9127F628Db9704FcdB097F162a1da2b46626BA"
     const USDSwap="0xDe403151C18C5A945A8eb028035abb7ced0D93A3"
@@ -15,7 +15,7 @@ export const DataProvider = ({ children }) => {
     const defaultInviter = "0xA263695d7487F16cb93655E90c9c7397d62cC30e"
     // const USDTContractAddress = "0x74C1C4a245A505F95ACB7D8ea4A4Ae63b0197259"
     // const USDContractAddress = "0x033435aC989Ef1f519BDA30033bCc6f9aef06bbF"
-    // const MinerContractAddress = "0x90D269Ab3dC66eF715Ce8aAf595BF45Bcb3D9b68"
+    // const MinerContractAddress = "0x7C57FFBb95570e07448767C5181AeffAEeabaC91"
 
     // const TEDAddress="0x34CF4625eD543C1680f95edC76FC1Ef4aD713C4A"
     // const USDSwap="0xa8f64CAB494A992563eE4D7a733d929876895ba3"
--- src/pages/statistics/index.jsx
+++ src/pages/statistics/index.jsx
@@ -6,53 +6,53 @@ import SummaryV2 from "../../component/summary/SummaryV2";
 import LocationV2 from "../../component/location";
 import TaskSummary from "../../component/summary/TaskSummary";
 import { useLocation } from 'react-router-dom';
-import { useEffect, useState } from "react";
 
 function Statistics() {
-  const [inviter, setInviter] = useState(null);
-  const [isLoaded, setIsLoaded] = useState(false);
-  const handleInviter = () => {
-    setIsLoaded(true);
-    const location = useLocation();
-    const searchParams = new URLSearchParams(location.search);
-    const tempInviter = searchParams.get('inviter');
-    setInviter(tempInviter);
-  }
+  const location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
+  const inviter = searchParams.get('inviter');
+  // const [isLoaded, setIsLoaded] = useState(false);
+  // const handleInviter = () => {
+  //   setIsLoaded(true);
+  //   const location = useLocation();
+  //   const searchParams = new URLSearchParams(location.search);
+  //   const tempInviter = searchParams.get('inviter');
+  //   setInviter(tempInviter);
+  // }
 
-  useEffect(() => {
-    if (isLoaded || inviter === null) return;
-    handleInviter()
-  }, [])
+  // useEffect(() => {
+  //   if (isLoaded || inviter === null) return;
+  //   handleInviter()
+  // }, [])
 
-  useEffect(() => {
-    const storeInviter = () => {
-      sessionStorage.setItem('inviter', inviter)
-    }
+  // useEffect(() => {
+  //   const storeInviter = () => {
+  //     sessionStorage.setItem('inviter', inviter)
+  //   }
 
-    const getInviter = () => {
-      try {
-        sessionStorage.getItem('inviter');
-      } catch (err) {
-        console.log(err)
-      }
-    }
+  //   const getInviter = () => {
+  //     try {
+  //       sessionStorage.getItem('inviter');
+  //     } catch (err) {
+  //       console.log(err)
+  //     }
+  //   }
 
-    if (inviter === null) {
-      getInviter()
-      return;
-    }
-    else
-      storeInviter();
+  //   if (inviter === null) {
+  //     getInviter()
+  //     return;
+  //   }
+  //   else
+  //     storeInviter();
 
-  }, [inviter])
-  
+  // }, [inviter])
   return (
     <main className="w-full px-6 pb-6 pt-[100px] sm:pt-[156px] xl:px-12 xl:pb-12">
       {/* write your code here */}
       <div className="2xl:flex 2xl:space-x-[48px]">
         <section className="2xl:flex-1 2xl:mb-0 mb-6">
           <div className="w-full mb-[24px] xl:flex xl:space-x-[24px]">
-            <SummaryV2 height="h-[180px]" inviter={inviter} />
+            <SummaryV2 height="h-[180px]" inviter={inviter}/>
           </div>
         </section>
         {/* <section className="flex w-full flex-col space-x-0 lg:flex-row lg:space-x-6 2xl:w-[400px] 2xl:flex-col 2xl:space-x-0">
